5f598ec367392bb13d9647f02621eb8786ff2a2f
Hian Claudio <hclaudio@labsobralnet.ind>
2025-10-23 14:15:33 -0300

    hml-3.5 - logs

--- backend/registro/apps.py
+++ backend/registro/apps.py
@@ -4,7 +4,3 @@ from django.apps import AppConfig
 class RegistroConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'registro'
-
-    def ready(self):
-        # importa o módulo que contém seus models fora do models.py
-        from .audit import audit  # noqa: F401
