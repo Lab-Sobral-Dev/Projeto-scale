99b6bdd3fded8fba4770572d9baf1d6ac08aeccc
Hian Claudio <hclaudio@labsobralnet.ind>
2025-10-23 14:11:23 -0300

    hml-3.3 - logs

--- backend/registro/serializers.py
+++ backend/registro/serializers.py
@@ -7,7 +7,7 @@ from .models import (
     OrdemProducao, ItemOP, Pesagem,
 )
 
-from registro.audit.audit import AuditLog
+from registro.audit_models import AuditLog
 
 
 # ============== Básicos ==============
